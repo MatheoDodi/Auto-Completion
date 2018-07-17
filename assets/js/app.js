8872749731d9a09411968b7292b5fd7001b28ce4
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-16 18:25:57 -0700

    added basic functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,43 @@ let cities = [];
 fetch(URL)
 .then(blob => blob.json()
 .then(data => cities = data));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+
+        let regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+        
+
+    })
+}
+
+function showMatches() {
+    if (this.value === "") {
+        suggestions.innerHTML = `<li>Filter for a city</li>
+        <li>or a state</li>`;
+    } else {
+
+        let matchesArray = findMatches(this.value, cities);
+        let html = matchesArray.map(item => {
+
+            let regex = new RegExp(this.value, 'gi');
+            let cityName = item.city.replace(regex, `<span class=hl>${this.value}</span>`)
+            let stateName = item.state.replace(regex, `<span class=hl>${this.value}</span>`)
+            return `<li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${item.population}</span>
+            </li>`
+        }).join('');
+
+        suggestions.innerHTML = html;
+
+    }
+
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change', showMatches);
+searchInput.addEventListener('keyup', showMatches);
